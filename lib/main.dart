48222782b52ad8c966b1f48a8800b836b84b4082
Lucas Farias <lukf95@gmail.com>
2020-01-26 23:20:21 -0300

    Stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+  runApp(PeregrineApp());
+}
+
+const AppTitle = "Peregrine";
+const HeaderActions = ["Profile", "Locations", "Rewards"];
+
+class PeregrineApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(AppTitle),
+        backgroundColor: Color.fromRGBO(40, 210, 40, 1),
       ),
-    ),
-  );
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: HeaderActions.map((String text) => RaisedButton(
+                  onPressed: () => {print("Pressed $text")},
+                  child: Text(text),
+                )).toList(),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(AppTitle,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                ))
+          ])
+        ],
+      ),
+    ));
+  }
 }
